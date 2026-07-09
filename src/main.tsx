129c685d62073cdeabe3233660767dc7a5675f8a
kamiql <kilianaqua08@gmail.com>
2026-07-09 14:44:50 +0200

    add initial project files and configure routing with react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router";
+import {HashRouter, Routes, Route} from "react-router-dom";
 import Banner from "./routes/banner/Banner.tsx";
-
-const router = createBrowserRouter([
-    {
-        path: "/banner",
-        element: <Banner/>
-    }
-])
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <HashRouter>
+            <Routes>
+                <Route path="/banner" element={<Banner/>}/>
+            </Routes>
+        </HashRouter>
     </StrictMode>,
 )
